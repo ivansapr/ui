7e66bb50e5d3c5e56ffb9dc2e4089b5de8907d42
ivansapr <kartoshka95@gmail.com>
2023-03-12 21:39:23 +0100

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:ui_exp/drop_down.dart';
+import 'package:ui_exp/following_letters.dart';
+import 'package:ui_exp/jumping_letters.dart';
 import 'package:ui_exp/rolling_numbers_widget.dart';
 
 void main() {
@@ -20,23 +23,56 @@ class _MainAppState extends State<MainApp> {
   @override
   void initState() {
     super.initState();
-    final r = Random();
-    numbers = r.nextInt(1);
+
+    numbers = 12345;
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          setState(() {
-            final r = Random();
-            numbers = r.nextInt(9999);
-          });
-        }),
-        body: Center(
-          child: Column(
-            children: [RollingNumbers(numbers: numbers), Text(numbers.toString())],
+      home: FollowingLetters(
+        body: Scaffold(
+          backgroundColor: Color.fromRGBO(27, 31, 36, 1),
+          body: Padding(
+            padding: EdgeInsets.only(top: 200),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Row(
+                  //   mainAxisSize: MainAxisSize.min,
+                  //   children: [
+                  //     RollingNumbers(numbers: numbers),
+                  //     TextButton(
+                  //         onPressed: () {
+                  //           setState(() {
+                  //             final r = Random();
+                  //             numbers = r.nextInt(9999);
+                  //           });
+                  //         },
+                  //         child: Text('Random')),
+                  //     TextButton(
+                  //         onPressed: () {
+                  //           setState(() {
+                  //             numbers += 1;
+                  //           });
+                  //         },
+                  //         child: Text('+1')),
+                  //   ],
+                  // ),
+                  // SizedBox(height: 10),
+                  // Row(
+                  //   mainAxisSize: MainAxisSize.min,
+                  //   children: [
+                  //     JumpingLetters(value: 'Ivan'),
+                  //   ],
+                  // ),
+                  // SizedBox(height: 10),
+                  CustomDropDown(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
